592c4e573c02231bedc8221c5366b86782790445
Nam Vo <namvo@Nams-MacBook-Pro.local>
2020-01-14 09:54:26 -0800

    Updated admin.py.

--- myblog/admin.py
+++ myblog/admin.py
@@ -3,8 +3,8 @@ from myblog.models import Post, Category
 
 class CategoryInline(admin.TabularInline):
     model = Category.posts.through
-    max_num = Category.objects.count() or 0
-    extra = Category.objects.count() or 0
+    # max_num = Category.objects.count() or 0
+    # extra = Category.objects.count() or 0
     
 class PostAdmin(admin.ModelAdmin):
     inlines = [
